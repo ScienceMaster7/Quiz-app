41206c034348a4aab2d3324da8beabbf59880523
Benedikt Walch <benx.bw@gmail.com>
2021-07-02 17:03:49 +0200

    If the the create button is clicked the tags are now displayed as well

--- create.js
+++ create.js
@@ -1,5 +1,6 @@
 const newQuestion = document.getElementById("Your-new-Question");
 const newAnswer = document.getElementById("your-New-Answer");
+const newTags = document.getElementById("your-New-Tags");
 const submitbutton = document.querySelector(".content__submit-button");
 const formElement = document.querySelector(".input-Area");
 
@@ -7,5 +8,6 @@ submitbutton.addEventListener("click", (event) => {
   event.preventDefault();
   console.log("Question: " + newQuestion.value);
   console.log("Answer: " + newAnswer.value);
+  console.log("Tag's: " + newTags.value);
   formElement.reset();
 });
