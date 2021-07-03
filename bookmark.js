bce3baf96ed763fc92bbfdade1d8c5c888844068
Benedikt Walch <benx.bw@gmail.com>
2021-07-03 10:49:57 +0200

    If I now click the bookmark icon on a quizcard in the main page it is appended to end of main

--- bookmark.js
+++ bookmark.js
@@ -1,7 +1,11 @@
 const bookmark = document.querySelectorAll(".bookmark--position");
+const quizcards = document.querySelectorAll(".quizcard");
+const mainElement = document.querySelector(".content");
 
 for (let i = 0; i < bookmark.length; i++) {
   bookmark[i].addEventListener("click", () => {
     bookmark[i].classList.toggle("bookmark--color--toggled");
+    let newQuizCard = quizcards[i];
+    mainElement.appendChild(newQuizCard);
   });
 }
