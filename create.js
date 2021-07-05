c5f63de60b4d820d7d9cabd2f1db1c5fae3ccf5f
Benedikt Walch <benx.bw@gmail.com>
2021-07-05 10:58:06 +0200

    Change const name submitbutton to createbutton

--- create.js
+++ create.js
@@ -1,10 +1,10 @@
 const newQuestion = document.getElementById("Your-new-Question");
 const newAnswer = document.getElementById("your-New-Answer");
 const newTags = document.getElementById("your-New-Tags");
-const submitbutton = document.querySelector(".content__submit-button");
+const createbutton = document.querySelector(".content__submit-button");
 const formElement = document.querySelector(".input-Area");
 
-submitbutton.addEventListener("click", (event) => {
+createbutton.addEventListener("click", (event) => {
   event.preventDefault();
   console.log("Question: " + newQuestion.value);
   console.log("Answer: " + newAnswer.value);
